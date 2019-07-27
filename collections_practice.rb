7a5bd80f0915ce9751650bf2c344c37d5be40b6d
assane savadogo <assane.savadogo81@gmail.com>
2019-07-27 04:32:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|name| name[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|w| w.start_with?("a")}
+end
+
+def sum_array(array)
+  array.reduce(:+) #{|sum, num| sum + num}
+end
+
+def array_s
